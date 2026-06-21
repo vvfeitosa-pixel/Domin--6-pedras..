b2cff6df63274d87e8959b666c5974dd02fab4f4
vitor <vvfeitosa@gmail.com>
2026-06-20 21:29:41 -0300

    Update main.tsx

--- artifacts/domino/src/main.tsx
+++ artifacts/domino/src/main.tsx
@@ -3,3 +3,12 @@ import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+// Força atualização do Service Worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => {
+      registration.update();
+    });
+  });
+}
